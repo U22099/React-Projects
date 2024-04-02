a39743ba61c013a749609b8960806a4894c15a31
Daniel <nifemiolaniyi4@gmail.com>
2024-04-02 16:03:08 +0100

    -----

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Projects/",
   plugins: [react()],
   assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
 })
