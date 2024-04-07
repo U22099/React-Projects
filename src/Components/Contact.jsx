d7ab4628e83b6b22c63802e08ab5f11e59e7bd52
Daniel <nifemiolaniyi4@gmail.com>
2024-04-07 22:47:25 +0100

    ---

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             <a className=" px-[20px] md:px-[40px] ml-[10px] mt-[10px] rounded-2xl bg-primary-color text-white shadow-purple-900 shadow-lg active:shadow-none flex justify-center items-center text-[1.2em]" href="mailto:nifemiolaniyi4@gmail.com"><MdOutlineAlternateEmail className="text-[1.2em] mr-[10px]"/> Daniel WebDev</a>
         </div>
         <form className="bg-gray-900 p-[20px] w-[300px] md:w-[350px] flex flex-col justify-center gap-[15px]" action="https://formsubmit.co/7cb8eae1ddf4e71a02dbf20c7eecde43" method="POST">
-            <input type="hidden" name='_next' value="https:/u22099.github.io/React-Projects/"/>
+            <input type="hidden" name='_next' value="https://u22099.github.io/React-Projects/"/>
             <input type="hidden" name="_captcha" value="false"/>
             <input className="bg-gray-800 shadow-[inset_10px_10px_70px_0px_rgba(0,0,0,0.8)] w-[260px] md:w-[300px] h-[60px] text-white rounded-lg p-[10px]" name="Name" type="text" placeholder='Name' required/>
             <input className="bg-gray-800 shadow-[inset_10px_10px_70px_0px_rgba(0,0,0,0.8)] w-260px] md:w-[300px] h-[60px] text-white rounded-lg p-[10px]" type="mail" name="Email Address" placeholder='Email' required/>
